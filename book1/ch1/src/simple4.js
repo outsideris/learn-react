43cccb1713c1824e4d9ec99836ac09b44e8cb0c2
Outsider <outsideris@gmail.com>
2022-07-19 03:12:17 +0900

    ch1: simple4
    Signed-off-by: Outsider <outsideris@gmail.com>

---
+++ book1/ch1/src/simple4.js
@@ -0,0 +1,26 @@
+function LikeButton() {
+  const [liked, setLiked] = React.useState(false);
+  const text= liked ? '좋아요 취소' : '좋아요';
+  return React.createElement(
+    'button',
+    { onClick: () => setLiked(!liked) },
+    text,
+  );
+}
+
+function Container() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <LikeButton />
+      <div style={{ marginTop: 20 }}>
+        <span>현재 카운트: </span>
+        <span>{count}</span>
+        <button onClick={() => setCount(count + 1)}>증가</button>
+        <button onClick={() => setCount(count - 1)}>감소</button>
+      </div>
+    </div>
+  );
+}
+const domContainer =document.querySelector('#react-root');
+ReactDOM.render(React.createElement(Container), domContainer);
