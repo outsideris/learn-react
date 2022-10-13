cddca65c9cb337a8ca5688c0cdbded8d935c1bdf
Outsider <outsideris@gmail.com>
2022-10-13 13:42:49 +0900

    ch1: init css-test to show class conflict
    Signed-off-by: Outsider <outsideris@gmail.com>

---
+++ book1/ch1/css-test/src/App.js
@@ -0,0 +1,15 @@
+import Button from "./Button1";
+import Box from "./Box1";
+
+function App() {
+  return (
+    <div>
+      <Button size="big" />
+      <Button size="small" />
+      <Box size="big" />
+      <Box siz="small" />
+    </div>
+  );
+}
+
+export default App;
